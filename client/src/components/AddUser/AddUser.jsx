6258fb5599b7364f31f484124d2dd61a2273e34b
rails course from udemy <lomaslove132@gmail.com>
2019-08-11 20:45:33 +1000

    add and edit pages fixed

--- client/src/components/AddUser/AddUser.jsx
+++ client/src/components/AddUser/AddUser.jsx
@@ -73,19 +73,7 @@ class AddUser extends Component {
             required
             id="age"
           />
-          <label htmlFor="email">Email: </label>
-          <input
-            type="text"
-            placeholder="Email"
-            name="email"
-            onChange={this.onChangeHandler}
-            ref="email"
-            className="Add-User-Input"
-            required
-            minLength=""
-            maxLength=""
-            id="email"
-          />
+          
           <button type="submit" className="Add-User-Submit fa fa-plus"></button>
           <button type="reset" className="Add-User-Reset fa fa-eraser"></button>
         </form>
